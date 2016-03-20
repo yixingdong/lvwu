252bf6c82ea9437fdb006c10cbbf86b9e8ecba73
yixingdong <team@exingdong.com>
2016-03-20 07:33:00 +0000

    微信账号与用户账号进行绑定

--- app/Http/Controllers/AuthThirdController.php
+++ app/Http/Controllers/AuthThirdController.php
@@ -46,6 +46,10 @@ class AuthThirdController extends Controller
             $user = Auth::user();
             //如果用户不是用微信登陆的，那就为其绑定微信号
             if(!$user->wx_id){
+                $result = User::where('wx_id',$wx_info['id'])->first();
+                if($result){
+                    return redirect('/')->withErrors('这个微信账号已被其他用户绑定，您不能绑定此微信账号');
+                }
                 $user->wx_id = $wx_info['id'];
                 $user->save();
                 return redirect('/')->withErrors('完成微信账号绑定');
@@ -55,25 +59,35 @@ class AuthThirdController extends Controller
         //2 没有登录
         $user = User::where('wx_id',$wx_info['id'])->first();
         //2.1 判断是否有此条记录，有则登录之
-        if(is_object($user)){
-            Auth::login($user);
-            switch ($user->type){
-                case 'lawyer':
-                case 'assist':
-                    return redirect('/')->withErrors('欢迎'.$user->type.'使用我们的法律平台');
-                case 'client':
-                    return redirect('/')->withErrors('欢迎咨询用户使用我们的服务');
-                default:
-                    return redirect('/')->withErrors('你还没有注册具体类型');
-            }
+        if(is_null($user)){
+            //2.2 没有此条记录，创建账户
+            $user = User::create([
+                'wx_id' => $wx_info['id']
+            ]);
         }
-        //2.2 没有此条记录，创建账户
-        $user = User::create([
-            'wx_id' => $wx_info['id']
-        ]);
 
         Auth::login($user);
-        return redirect('/')->withErrors('登录成功，您好没有注册具体类型');
+
+        switch ($user->type){
+            case 'lawyer':
+            case 'assist':
+                return redirect('/')->withErrors('欢迎'.$user->type.'使用我们的法律平台');
+            case 'client':
+                return redirect('/')->withErrors('欢迎咨询用户使用我们的服务');
+            default:
+                return redirect('/')->withErrors('你还没有注册具体类型');
+        }
+    }
+
+    public function releaseWeChatAccount()
+    {
+        if(Auth::check()){
+            $user = Auth::user();
+            if($user->wx_id){
+                $user->wx_id = null;
+            }
+            return redirect('/')->withErrors('已经解除了原来的微信账号绑定');
+        }
     }
 
     public function bindUserByWeChat()
