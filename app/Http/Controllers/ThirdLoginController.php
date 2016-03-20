0e38d6ca1266f81c39f2520be1b9cea94ea8e46b
yixingdong <team@exingdong.com>
2016-03-20 02:55:04 +0000

    modified

--- app/Http/Controllers/ThirdLoginController.php
+++ app/Http/Controllers/ThirdLoginController.php
@@ -12,6 +12,29 @@ use \Socialite;
 
 class ThirdLoginController extends Controller
 {
+
+    public function wxLogin()
+    {
+        echo 'I am going to WeiChat Login page';
+        return  Socialite::driver('wechat')->redirect();
+    }
+
+    public function wxCallback()
+    {
+        echo "I am WeChat provider callback handler";
+        $user = Socialite::driver('wechat')->user();
+        dd($user['id']);
+    }
+
+    public function wxCheck(Server $server)
+    {
+        $server->on('message', function($message){
+            return "欢迎关注 overtrue！";
+        });
+
+        return $server->serve(); // 或者 return $server;
+    }
+
     public function isUserExist($wx_id)
     {
         $user = User::where('wx_id',$wx_id)->first();
@@ -25,10 +48,10 @@ class ThirdLoginController extends Controller
         Auth::login($user);
         return redirect('/user/profile/create');
         //1 判断账号是否存在
-            //true 登录并跳转到指定页
-            //false
+        //true 登录并跳转到指定页
+        //false
     }
-    
+
     public function qqLogin()
     {
         echo 'I am going to QQ Login Page';
@@ -54,27 +77,5 @@ class ThirdLoginController extends Controller
         echo "I am WeiBo provider callback handler";
         $user = Socialite::driver('weibo')->user();
         dd($user);
-    }
-
-    public function wxLogin()
-    {
-        echo 'I am going to WeiChat Login page';
-        return  Socialite::driver('wechat')->redirect();
-    }
-
-    public function wxCallback()
-    {
-        echo "I am WeChat provider callback handler";
-        $user = Socialite::driver('wechat')->user();
-        dd($user);
-    }
-
-    public function wxCheck(Server $server)
-    {
-        $server->on('message', function($message){
-            return "欢迎关注 overtrue！";
-        });
-
-        return $server->serve(); // 或者 return $server;
     }
 }
