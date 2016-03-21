df489b02bcb8fa309469148586e526cf29aa69f6
yixingdong <team@exingdong.com>
2016-03-21 07:11:03 +0000

    第三方登录

--- app/Http/Controllers/AuthThirdsController.php
+++ app/Http/Controllers/AuthThirdsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Socialite;
+use Overtrue\LaravelSocialite\Socialite;
 use Illuminate\Support\Facades\Auth;
 use App\User;
 
