23d56bdc1f23602f5b4a5e85de7e12dd31c78535
yixingdong <team@exingdong.com>
2016-03-21 06:37:23 +0000

    第三方登录

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,19 +31,19 @@ Route::get('reset/email/{token}', 'Auth\PasswordController@getEmailReset');
 Route::post('reset/email/confirmed', 'Auth\PasswordController@postEmailReset');
 
 Route::group(['prefix' => 'thirds'], function(){
-    Route::get('/wx/login','AuthThirdController@wxLogin');
-    Route::get('/wx/callback','AuthThirdController@wxCallback');
-    Route::get('/qq/login','AuthThirdController@qqLogin');
-    Route::get('/qq/callback','AuthThirdController@qqCallback');
-    Route::get('/wb/login','AuthThirdController@wbLogin');
-    Route::get('/wb/callback','AuthThirdController@wbCallback');
+    Route::get('/wx/login','AuthThirdsController@wxLogin');
+    Route::get('/wx/callback','AuthThirdsController@wxCallback');
+    Route::get('/qq/login','AuthThirdsController@qqLogin');
+    Route::get('/qq/callback','AuthThirdsController@qqCallback');
+    Route::get('/wb/login','AuthThirdsController@wbLogin');
+    Route::get('/wb/callback','AuthThirdsController@wbCallback');
 
-    Route::get('chose','AuthThirdController@getChoseType');
-    Route::post('chose','AuthThirdController@postChoseType');
-    Route::get('bind','AuthThirdController@getBindExistUser');
-    Route::post('bind','AuthThirdController@postBindExistUser');
-    Route::get('new','AuthThirdController@getBindNewUser');
-    Route::post('new','AuthThirdController@postBindNewUser');
+    Route::get('chose','AuthThirdsController@getChoseType');
+    Route::post('chose','AuthThirdsController@postChoseType');
+    Route::get('bind','AuthThirdsController@getBindExistUser');
+    Route::post('bind','AuthThirdsController@postBindExistUser');
+    Route::get('new','AuthThirdsController@getBindNewUser');
+    Route::post('new','AuthThirdsController@postBindNewUser');
 });
 
 Route::group(['prefix' => 'tool'], function(){
