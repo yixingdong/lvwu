84ef2e5b746b05f228b90da61a29070a9cf79755
yixingdong <team@exingdong.com>
2016-03-21 09:31:50 +0000

    第三方登录

--- app/Http/Controllers/AuthWeChatController.php
+++ app/Http/Controllers/AuthWeChatController.php
@@ -32,6 +32,7 @@ class AuthWeChatController extends Controller
     public function wxCallback()
     {
         echo "I am WeChat provider callback handler";
+        dd(Socialite::driver('wechat')->stateless());
         try{
             $wx_user = Socialite::driver('wechat')->user();
         }catch (Exception $e){
