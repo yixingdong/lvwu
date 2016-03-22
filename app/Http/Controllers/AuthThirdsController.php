0ebe14a2d2afb730a020e546199379bc294d8889
yixingdong <team@exingdong.com>
2016-03-22 01:15:45 +0000

    数据跟踪系统；

--- app/Http/Controllers/AuthThirdsController.php
+++ app/Http/Controllers/AuthThirdsController.php
@@ -45,12 +45,12 @@ class AuthThirdsController extends Controller
             $user = User::create([
                 'wx_id' => $wx_info['id'],
                 'type'  => 'undefined',
-                'avatar' => $wx_info['avatar'],
-
+                'avatar' => $wx_info['avatar']
             ]);
         }
 
         Auth::login($user);
+
 
         switch ($user->type){
             case 'lawyer':
@@ -59,7 +59,7 @@ class AuthThirdsController extends Controller
             case 'client':
                 return redirect('/')->withErrors('欢迎咨询用户使用我们的服务');
             case 'undefined':
-                return redirect('thirds/chose',compact('user'));
+                return redirect('thirds/chose');
             default:
                 return redirect('/')->withErrors('您的信息已被记录，恶意攻击将被记录在案');
         }
