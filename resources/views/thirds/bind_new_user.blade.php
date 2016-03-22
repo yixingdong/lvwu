9c60ea109e2041a87bb60b8c9e1cf7064b5122ab
yixingdong <team@exingdong.com>
2016-03-22 01:23:00 +0000

    数据跟踪系统；

--- resources/views/thirds/bind_new_user.blade.php
+++ resources/views/thirds/bind_new_user.blade.php
@@ -11,7 +11,7 @@
             </div>
         </div>
         <!-- form start -->
-        <form class="form-horizontal" method="POST" action="{{URL('/third/new')}}">
+        <form class="form-horizontal" method="POST" action="{{URL('/thirds/new')}}">
             {!! csrf_field() !!}
             <input type="hidden" name="todo" value="reg">
             <div class="box-body">
