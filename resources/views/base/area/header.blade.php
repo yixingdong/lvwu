7e0a23c44455dd3fd58040d3fb62ab427f396fe6
yixingdong <team@exingdong.com>
2016-03-23 13:54:35 +0000

    微信登陆

--- resources/views/base/area/header.blade.php
+++ resources/views/base/area/header.blade.php
@@ -85,7 +85,7 @@
                 @else
                     <li><a href="{{URL('chose')}}">注册</a></li>
                     <li><a href={{URL('login')}}>登录</a></li>
-                    <li><a href={{URL('wx/login')}}>扫码登录</a></li>
+                    <li><a href={{URL('wx/login')}}>扫码登注</a></li>
                 @endif
             </ul>
         </div>
